b6da5c4966109e18ff922647fdb02520fbf0399f
Jack Atherton <atherton.lawrence@gmail.com>
2019-05-06 14:01:41 -0700

    non-vr non-textured terrain by example

---
+++ Assets/Scripts/RapidMix/RapidMixRegression.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class RapidMixRegression : MonoBehaviour
+{
+    private System.UInt32 myTrainingID, myRegressionID, myOutputLength;
+    private bool haveTrained = false;
+
+    void Awake()
+    {
+        myTrainingID = createEmptyTrainingData();
+        myRegressionID = createNewStaticRegression();
+        myOutputLength = 0;
+    }
+
+    public void RecordDataPoint( double[] input, double[] output )
+    {
+        // remember expected output length
+        if( myOutputLength == 0 )
+        {
+            myOutputLength = (System.UInt32) output.Length;
+        }
+
+        // show error if we get something that isn't the expected length
+        if( myOutputLength != output.Length )
+        {
+            Debug.LogError( string.Format( "Received output of dimension {0} which was different than the expected / originally recieved output dimension {1}", output.Length, myOutputLength ) );
+        }
+
+        recordSingleTrainingElement(
+            myTrainingID,
+            input, (System.UInt32) input.Length,
+            output, (System.UInt32) output.Length
+        );
+    }
+
+    public void Train()
+    {
+        trainStaticRegression( myRegressionID, myTrainingID );
+        haveTrained = true;
+    }
+
+    public double[] Run( double[] input )
+    {
+        if( !haveTrained )  
+        {
+            Debug.LogError( "Regression can't Run() without having Train()ed first!" );
+            return new double[]{ };
+        }
+        double [] output = new double[myOutputLength];
+        runStaticRegression(
+            myRegressionID,
+            input, (System.UInt32) input.Length,
+            output, (System.UInt32) output.Length
+        );
+        return output;
+    }
+
+    // TODO can the dataset also be reset? need to check C++ API
+    public void ResetRegression()
+    {
+        resetStaticRegression( myRegressionID );
+        haveTrained = false;
+        // myOutputLength = 0; // to enable if the dataset can also be reset
+    }
+
+    const string PLUGIN_NAME = "RapidMixAPI";
+
+    [DllImport( PLUGIN_NAME )]
+    private static extern System.UInt32 createEmptyTrainingData();
+
+    [DllImport( PLUGIN_NAME )]
+    private static extern System.UInt32 createNewStaticRegression();
+
+    [DllImport( PLUGIN_NAME )]
+    private static extern bool recordSingleTrainingElement(
+        System.UInt32 trainingID,
+        double[] input, System.UInt32 n_input,
+        double[] output, System.UInt32 n_ouput
+    );
+
+    [DllImport( PLUGIN_NAME )]
+    private static extern bool trainStaticRegression( System.UInt32 regressionID, System.UInt32 trainingID );
+
+    [DllImport( PLUGIN_NAME )]
+    private static extern bool runStaticRegression(
+        System.UInt32 regressionID,
+        double[] input, System.UInt32 n_input,
+        double[] output, System.UInt32 n_output
+    );
+
+    [DllImport( PLUGIN_NAME )]
+    private static extern bool resetStaticRegression( System.UInt32 regressionID );
+
+}
---
+++ Assets/Scripts/TerrainController.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainController : MonoBehaviour
+{
+
+    public GameObject topLand;
+    public GameObject bottomLand;
+    public GameObject vertexDebugPrefab;
+
+    public Transform examplePointsContainer;
+
+    private int verticesPerSide = 101;
+    private float landSize;
+    private float spaceBetweenVertices;
+    public bool showDebugMarkers = false;
+
+    // regression
+    private RapidMixRegression myRegression;
+
+    public void ProvideExample( Vector3 givenHeight )
+    {
+        // world to local point
+        //givenHeight = givenHeight - transform.position; 
+        givenHeight = transform.InverseTransformPoint( givenHeight );
+        myRegression.RecordDataPoint( InputVector( givenHeight.x, givenHeight.z ), new double[] { givenHeight.y } );
+        myRegression.Train();
+        ComputeLandHeight( topLand, false );
+        ComputeLandHeight( bottomLand, true );
+    }
+
+    private double[] InputVector( float x, float z )
+    {
+        // kernel method
+        return new double[] { x, z, x*x, z*z, x*z, x*x*x, z*z*z, x*x*z, x*z*z };
+        // return new double[] { x, z, x*x, z*z, x*z };
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // grab component reference
+        myRegression = GetComponent<RapidMixRegression>();
+
+        // compute sizes
+        landSize = 10; // it is invariant to scale. scaling up doesn't affect the computations here.
+        spaceBetweenVertices = landSize / ( verticesPerSide - 1 );
+        Debug.Log( spaceBetweenVertices );
+        Debug.Log( IndicesToCoordinates( 0, 0 )) ;
+
+
+        // construct meshes
+        ConstructMesh( topLand );
+        ConstructMesh( bottomLand );
+
+
+        if( showDebugMarkers )
+        {
+            CreateDebugMarkers();
+        }
+
+        foreach( Transform example in examplePointsContainer )
+        {
+            ProvideExample( example.position );
+        }
+        
+    }
+    
+    void CreateDebugMarkers()
+    {
+        Mesh m = topLand.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = m.vertices;
+        // for( int i = 0; i < vertices.Length; i++ )
+        // {
+        //     Instantiate( vertexDebugPrefab, new Vector3( vertices[i].x, 2, vertices[i].z), Quaternion.identity );
+        // }
+        Instantiate( vertexDebugPrefab, new Vector3( -spaceBetweenVertices, 0, -spaceBetweenVertices ), Quaternion.identity);
+        Instantiate( vertexDebugPrefab, new Vector3( -spaceBetweenVertices, 0, spaceBetweenVertices ), Quaternion.identity);
+        Instantiate( vertexDebugPrefab, new Vector3( spaceBetweenVertices, 0, -spaceBetweenVertices ), Quaternion.identity);
+        Instantiate( vertexDebugPrefab, new Vector3( spaceBetweenVertices, 0, spaceBetweenVertices ), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private Vector3 IndicesToCoordinates( int x, int z )
+    {
+        // 0, 0 is bottom left corner, NOT center
+        return new Vector3( ( x - ( verticesPerSide / 2 ) ) * spaceBetweenVertices, 0, ( z - ( verticesPerSide / 2 ) ) * spaceBetweenVertices );
+    }
+
+    // // unused?
+    // private Vector2Int CoordinatesToClosestIndices( Vector3 coords )
+    // {
+    //     int bestX = (int) ( coords.x / spaceBetweenVertices ) + verticesPerSide / 2;
+    //     int bestZ = (int) ( coords.z / spaceBetweenVertices ) + verticesPerSide / 2;
+
+    //     return new Vector2Int( bestX, bestZ );
+    // }
+
+    private void ConstructMesh( GameObject o )
+    {
+        Mesh mesh = new Mesh();
+        o.GetComponent<MeshFilter>().mesh = mesh;
+        Vector3[] newVertices = new Vector3[verticesPerSide * verticesPerSide];
+        Vector2[] newUVs = new Vector2[verticesPerSide * verticesPerSide];
+        int[] newTriangles = new int[3 * 2 * ( 2 * verticesPerSide ) * ( 2 * verticesPerSide )];
+        // vertices
+        for( int x = 0; x < verticesPerSide; x++ )
+        {
+            for( int z = 0; z < verticesPerSide; z++ )
+            {
+                newVertices[x + z * verticesPerSide] = IndicesToCoordinates( x, z );
+                newUVs[x + z * verticesPerSide] = new Vector2( x * 1.0f / verticesPerSide, z * 1.0f / verticesPerSide );
+            }
+        }
+        // triangles
+        int triangleIndex = 0;
+        for( int x = 0; x < verticesPerSide - 1; x++ )
+        {
+            for( int z = 0; z < verticesPerSide - 1; z++ )
+            {
+                newTriangles[triangleIndex] = x + z * verticesPerSide; triangleIndex++;
+                newTriangles[triangleIndex] = x + 1 + z * verticesPerSide; triangleIndex++;
+                newTriangles[triangleIndex] = x + ( z + 1 ) * verticesPerSide; triangleIndex++;
+
+                newTriangles[triangleIndex] = x + 1 + z * verticesPerSide; triangleIndex++;
+                newTriangles[triangleIndex] = x + 1 + ( z + 1 ) * verticesPerSide; triangleIndex++;
+                newTriangles[triangleIndex] = x + ( z + 1 ) * verticesPerSide; triangleIndex++;
+            }
+        }
+        mesh.vertices = newVertices;
+        mesh.uv = newUVs;
+        mesh.triangles = newTriangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void ComputeLandHeight( GameObject o, bool reverseHeight )
+    {
+        Mesh m = o.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = m.vertices;
+
+        // recompute height
+        for( int i = 0; i < vertices.Length; i++ )
+        {
+            vertices[i].y = (float) myRegression.Run( InputVector( vertices[i].x, vertices[i].z ) )[0];
+        }
+
+        if( reverseHeight )
+        {
+            for( int i = 0; i < vertices.Length; i++ )
+            {
+                vertices[i].y *= -1;
+            }
+        }
+
+        // set vertices
+        m.vertices = vertices;
+        m.RecalculateNormals();
+    }
+
+
+
+}

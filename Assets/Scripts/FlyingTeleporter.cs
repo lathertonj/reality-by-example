168fc70025475f749a410fb088587b4607298685
Jack Atherton <atherton.lawrence@gmail.com>
2019-05-06 17:19:07 -0700

    naive teleportation implemented; will work better I think if switch to putting hand in new location

--- Assets/Scripts/FlyingTeleporter.cs
+++ Assets/Scripts/FlyingTeleporter.cs
@@ -17,11 +17,19 @@ public class FlyingTeleporter : MonoBehaviour
 
     public float minTeleport, maxTeleport;
 
+    public Transform room, head;
+    public GameObject teleportLaserEndPrefab;
+    private GameObject teleportLaserEnd;
+
+
     // Start is called before the first frame update
     void Start()
     {
         laser = Instantiate( laserPrefab );
         laserTransform = laser.transform;
+        teleportLaserEnd = Instantiate( teleportLaserEndPrefab );
+        laser.SetActive( false );
+        teleportLaserEnd.SetActive( false );
     }
 
     // Update is called once per frame
@@ -29,17 +37,17 @@ public class FlyingTeleporter : MonoBehaviour
     {
         if( ClickDown() )
         {
-            Debug.Log("click down!");
+            //Debug.Log("click down!");
         }
 
         if( ClickCurrentlyDown() )
         {
-            ShowLaser( GetLaserLength() );
+            ShowLaser();
         }
 
         if( ClickUp() )
         {
-            Teleport( GetLaserLength() );
+            Teleport();
         }
     }
 
@@ -48,18 +56,37 @@ public class FlyingTeleporter : MonoBehaviour
         return touchpadXY.GetAxis( handType ).y.Map( -1, 1, minTeleport, maxTeleport );
     }
 
-    private void ShowLaser( float length )
+    private Vector3 GetTeleportPosition()
     {
-        laser.SetActive( true );
-        Vector3 endpoint = controllerPose.transform.position + length * controllerPose.transform.forward;
-        laserTransform.position = Vector3.Lerp( controllerPose.transform.position, endpoint, 0.5f );
-        laserTransform.LookAt( endpoint );
-        laserTransform.localScale = new Vector3( laserTransform.localScale.x, laserTransform.localScale.y, length );
+        return controllerPose.transform.position + GetLaserLength() * controllerPose.transform.forward;
     }
 
-    private void Teleport( float length )
+    private void ShowLaser()
     {
+        // show the laser
+        laser.SetActive( true );
+
+        // make the laser the right length and orientation
+        Vector3 endpoint = GetTeleportPosition();
+        laserTransform.position = Vector3.Lerp( controllerPose.transform.position, endpoint, 0.5f );
+        laserTransform.LookAt( endpoint );
+        laserTransform.localScale = new Vector3( laserTransform.localScale.x, laserTransform.localScale.y, GetLaserLength() );
+
+        // show the laser end bit
+        teleportLaserEnd.SetActive( true );
+        teleportLaserEnd.transform.position = endpoint;
+    }
+
+    private void Teleport()
+    {
+        // hide lasers
         laser.SetActive( false );
+        teleportLaserEnd.SetActive( false );
+
+        // we want to put the user's body in this position, not the center of the room
+        Vector3 headRoomOffset = room.position - head.position;
+        headRoomOffset.y = 0;
+        room.position = GetTeleportPosition() + headRoomOffset;
     }
 
     private bool ClickDown()

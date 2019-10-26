a8238b31061cc1b796f485d41c7a200a33a92a1d
Jack Atherton <atherton.lawrence@gmail.com>
2019-10-25 17:07:35 -0700

    some wholly untested skeleton code for the sound engine

---
+++ Assets/Scripts/SoundEngine.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEngine : MonoBehaviour
+{
+
+    ChuckSubInstance myChuck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myChuck = GetComponent<ChuckSubInstance>();
+        myChuck.RunCode( @"
+            global int whichChords;
+            global float timbreSlider;
+            global float densitySlider;
+            global float quarterNoteTempoSeconds;
+            global float volumeSlider;
+
+            global Event quarterNoteHappened, eighthNoteHappened, sixteenthNoteHappened;
+            fun void SendTempoEvents()
+            {
+                while( true )
+                {
+                    quarterNoteHappened.broadcast();
+                    eighthNoteHappened.broadcast();
+                    sixteenthNoteHappened.broadcast();
+                    (quarterNoteTempoSeconds / 4)::second => now;
+
+                    sixteenthNoteHappened.broadcast();
+                    (quarterNoteTempoSeconds / 4)::second => now;
+
+                    eighthNoteHappened.broadcast();
+                    sixteenthNoteHappened.broadcast();
+                    (quarterNoteTempoSeconds / 4)::second => now;
+
+                    sixteenthNoteHappened.broadcast();
+                    (quarterNoteTempoSeconds / 4)::second => now;
+                }
+            }
+            spork ~ SendTempoEvents();
+
+
+            // G is the lowest root note; F# root starts ABOVE middle C
+            55 => int G;
+            56 => int Gs;
+            57 => int A;
+            58 => int As;
+            59 => int B;
+            60 => int C;
+            61 => int Cs;
+            62 => int D;
+            63 => int Ds;
+            64 => int E;
+            65 => int F;
+            66 => int Fs;
+            [ 
+                // V:  AM7 x2, EM7 x2
+                [ [A, Cs+12, E,     Gs+12],  [A, Cs+12, E,    Gs+12], 
+                  [E, B+12,  Ds+12, Gs+12], [E, B+12,  Ds+12, Gs+12] ],
+                // V 2: AM7, EM7, g#7, g#7 sus4
+                [ [A,  Cs+12, E,     Gs+12], 
+                  [E,  B+12,  Ds+12, Gs+12],
+                  [Gs, B+12,  Ds+12, Fs],
+                  [Gs, Cs+12, Ds+12, Fs] ],
+                // VII: f#, DM7, c#7, f#sus2
+                [ [Fs, A+12,  Cs+12, Fs+12],
+                  [D,  A+12,  Cs+12, Fs+12],
+                  [Cs, Gs+12, B+12,  E+12],
+                  [Fs, Gs+12, Cs+12, Fs+12] ],
+                // I: b, f#7, GM7, DM7
+                [ [B,  Fs,   B+12,  D+12], 
+                  [Fs, A+12, Cs+12, E+12],
+                  [G,  B+12, D+12,  Fs+12],
+                  [D,  A+12, Cs+12, Fs+12] ],
+                // IIIB/VIII: A+9+13, f#sus4, AM7sus2, E
+                [ [A,  Cs+12, B+12, Fs+12],
+                  [Fs, Cs+12, B+12, Fs+12], 
+                  [A,  E+12,  B+12, Gs+24], 
+                  [E,  E+12,  B+12, Gs+24S] ],
+                // IV: f#7+11 x2, E+9sus4 x2
+                [ [Fs, A+12, E+12,  B+24], [Fs, A+12, E+24,  B+24], 
+                  [E,  A+12, Fs+12, B+24], [E,  A+12, Fs+24, B+24] ]
+            ] @=> int[][][] theChords;
+            global int[4] currentChord;
+
+            fun void PopulateCurrentChord()
+            {
+                int currentChord;
+                while( true )
+                {
+                    
+                    for( int i; i < currentChord.size(); i++ )
+                    {
+                        theChords[whichChords][currentChord][i] => currentChord[i];
+                    }
+                    repeat(4) { quarterNoteHappened => now; }
+                    currentChord++;
+                    currentChord % 4 => currentChord;
+                }
+            }
+            spork ~ PopulateCurrentChord();
+
+
+            fun void ControlVolume()
+            {
+                while( true )
+                {
+                    volumeSlider => dac.gain;
+                    10::ms => now;
+                }
+            }
+            spork ~ ControlVolume();
+            while( true ) { 1::second => now; }
+        " ); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetChord( int whichChord )
+    {
+        myChuck.SetInt( "whichChord", whichChord );
+    }
+
+    public void SetTimbre( float zeroToOne )
+    {
+        myChuck.SetFloat( "timbreSlider", zeroToOne );
+    }
+
+    public void SetDensity( float zeroToOne )
+    {
+        myChuck.SetFloat( "densitySlider", zeroToOne );
+    }
+
+    public void SetVolume( float zeroToOne )
+    {
+        myChuck.SetFloat( "volumeSlider", zeroToOne );
+    }
+
+    public void SetQuarterNoteTime( float inSeconds )
+    {
+
+    }
+
+    public void SetSong( int TODOWHATARGS )
+    {
+
+    }
+
+
+}

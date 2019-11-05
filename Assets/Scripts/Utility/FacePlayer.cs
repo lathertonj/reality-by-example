eeee0a1b37234204638d4989fec32cb491da1cab
Jack Atherton <atherton.lawrence@gmail.com>
2019-11-05 15:34:38 -0800

    make prefab labels face the player

---
+++ Assets/Scripts/Utility/FacePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( FaceTheTransform() );
+    }
+
+    IEnumerator FaceTheTransform()
+    {
+        float updateTime = Random.Range( 0.05f, 0.1f );
+        float damping = 0.5f;
+
+        while( true )
+        {
+            yield return new WaitForSecondsRealtime( updateTime );
+
+            var lookPos = ThePlayer.theTransform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation( lookPos );
+            transform.rotation = Quaternion.Slerp( transform.rotation, rotation, damping );
+        }
+    }
+
+}
---
+++ Assets/Scripts/Utility/ThePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThePlayer : MonoBehaviour
+{
+    public static Transform theTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theTransform = transform;
+    }
+}

c1c768369c643cc4b7ee56e25f602f98da1ba9b9
Jack Atherton <atherton.lawrence@gmail.com>
2020-06-10 17:50:51 -0700

    movement with arrow keys and rotation with right click drags

---
+++ Assets/Scripts/Utility/ArrowKeyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowKeyMovement : MonoBehaviour
+{
+    public float movementPerSecond = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+        if( Input.GetKey( KeyCode.LeftArrow ) )
+        {
+            direction += -transform.right;
+        }
+        if( Input.GetKey( KeyCode.RightArrow ) )
+        {
+            direction += transform.right;
+        }
+        if( Input.GetKey( KeyCode.UpArrow ) )
+        {
+            direction += transform.forward;
+        }
+        if( Input.GetKey( KeyCode.DownArrow ) )
+        {
+            direction += -transform.forward;
+        }
+
+        if( direction.magnitude > 0.01f )
+        {
+            transform.position += movementPerSecond * Time.deltaTime * direction.normalized;
+        }
+    }
+}

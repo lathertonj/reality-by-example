7b2d232c8865eee1845678faf994a81debd55f2c
Jack Atherton <atherton.lawrence@gmail.com>
2020-06-12 12:23:22 -0700

    enable option to follow an object but not quite all the way

--- Assets/Scripts/VUI/SlewToTransform.cs
+++ Assets/Scripts/VUI/SlewToTransform.cs
@@ -9,6 +9,9 @@ public class SlewToTransform : MonoBehaviour
 
     public bool slewYRotation = true;
 
+    public bool keepDistance = false;
+    public float distanceToKeep = 1f;
+
     void Start()
     {
 
@@ -17,11 +20,25 @@ public class SlewToTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += slewSeconds * Time.deltaTime * ( objectToTrack.position - transform.position );
+        transform.position += slewSeconds * Time.deltaTime * ( GoalPosition() - transform.position );
         if( slewYRotation )
         {
             Quaternion goalRotation = Quaternion.AngleAxis( objectToTrack.eulerAngles.y, Vector3.up );
             transform.rotation = Quaternion.Slerp( transform.rotation, goalRotation, slewSeconds * Time.deltaTime );
+        }
+    }
+
+    Vector3 GoalPosition()
+    {
+        if( keepDistance )
+        {
+            // the place to go to is actually most, but not all of the way toward object
+            Vector3 objectToMe = transform.position - objectToTrack.position;
+            return objectToTrack.position + distanceToKeep * objectToMe.normalized;
+        }
+        else
+        {
+            return objectToTrack.position;
         }
     }
 }

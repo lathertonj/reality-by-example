6572dd54df4493f309354662cd99da81f4793fb5
Jack Atherton <atherton.lawrence@gmail.com>
2021-01-14 15:50:55 -0800

    photon creating room and spawning an avatar in single player working

---
+++ Assets/Scripts/Photon/InstantiatePhotonAvatar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class InstantiatePhotonAvatar : MonoBehaviourPunCallbacks
+{
+    public GameObject avatarPrefab;
+    
+    public override void OnJoinedRoom()
+    {
+        GameObject newAvatar = PhotonNetwork.Instantiate( avatarPrefab.name, transform.position, Quaternion.identity );
+        newAvatar.transform.parent = transform;
+        newAvatar.transform.localPosition = Vector3.zero;
+        newAvatar.transform.localRotation = Quaternion.identity;
+    }
+    
+}

ded835ee4a0adc0cfbb9b50988bac2a25b94c2a0
Jack Atherton <atherton.lawrence@gmail.com>
2021-02-12 14:46:02 -0800

    skeleton of creature color sync

---
+++ Assets/Scripts/Photon/PhotonAnimatedCreatureColorView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonAnimatedCreatureColorView : MonoBehaviour , IPunObservable
+{
+    AnimatedCreatureColor myColor;
+    void Awake()
+    {
+        myColor = GetComponent<AnimatedCreatureColor>();
+    }
+    
+    void IPunObservable.OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )
+    {
+        // Write to others
+        if (stream.IsWriting)
+        {
+            // send the color
+            stream.SendNext( myColor.Serialize() );
+        }
+        // Read from others
+        else
+        {
+            // receive the color
+            myColor.Deserialize( (float) stream.ReceiveNext() );
+        }
+    }
+
+}

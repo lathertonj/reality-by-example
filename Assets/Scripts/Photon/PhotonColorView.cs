a5d31d143001c27cfc3b3c54b6e0d3bc91804c3d
Jack Atherton <atherton.lawrence@gmail.com>
2021-02-18 15:25:41 -0800

    test for laser selection enabled

---
+++ Assets/Scripts/Photon/PhotonColorView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonColorView : MonoBehaviour , IPunObservable
+{
+
+    MeshRenderer me;
+    // Start is called before the first frame update
+    void Start()
+    {
+        me = GetComponent<MeshRenderer>();
+    }
+
+    void IPunObservable.OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )
+    {
+        // Write to others
+        if (stream.IsWriting)
+        {
+            Color c = me.material.color;
+            stream.SendNext( c );
+        }
+        // Read from others
+        else
+        {
+            Color c = (Color) stream.ReceiveNext();
+            me.material.color = c;
+        }
+    }
+}
---
+++ Assets/Scripts/Photon/PhotonEnabledView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonEnabledView : MonoBehaviour , IPunObservable
+{
+    void IPunObservable.OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )
+    {
+        // Write to others
+        if (stream.IsWriting)
+        {
+            // is the game object active?
+            bool a = gameObject.activeSelf;
+            stream.SendNext( a );
+        }
+        // Read from others
+        else
+        {
+            bool a = (bool) stream.ReceiveNext();
+            gameObject.SetActive( a );
+        }
+    }
+}

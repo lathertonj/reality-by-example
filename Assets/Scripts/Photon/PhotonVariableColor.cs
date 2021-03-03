764538e3ac65b3d30e6169c8c9503b5a86cf2a3a
Jack Atherton <atherton.lawrence@gmail.com>
2021-03-03 15:36:41 -0800

    different color for others' selection

---
+++ Assets/Scripts/Photon/PhotonVariableColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonVariableColor : MonoBehaviour , IPunInstantiateMagicCallback
+{
+    public Color myColor, othersColor;
+
+    void IPunInstantiateMagicCallback.OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        ParticleSystem.MainModule particles = GetComponent<ParticleSystem>().main;
+        if( GetComponent<PhotonView>().IsMine )
+        {
+            particles.startColor = myColor;
+        }
+        else
+        {
+            particles.startColor = othersColor;
+        }
+    }
+
+}

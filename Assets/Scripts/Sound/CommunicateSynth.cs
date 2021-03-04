efedaaea75881798196b20d7370010165785fea6
Jack Atherton <atherton.lawrence@gmail.com>
2021-03-04 15:33:23 -0800

    first manual mapping

---
+++ Assets/Scripts/Sound/CommunicateSynth.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommunicateSynth : MonoBehaviour
+{
+
+    // TODO: make this class dynamically via photon
+    string myPitch, myAmplitude, myTimbre;
+    float currentPitch, currentAmplitude, currentTimbre;
+    bool amOn;
+    ChuckMainInstance myChuck;
+    void Start()
+    {
+        myChuck = TheChuck.instance;
+        myPitch = myChuck.GetUniqueVariableName( "pitch" );
+        myAmplitude = myChuck.GetUniqueVariableName( "amplitude" );
+        myTimbre = myChuck.GetUniqueVariableName( "timbre" );
+        myChuck.RunCode( string.Format( @"
+            TriOsc t => LPF l => dac;
+            100 => float currentFreq => global float {0};
+            0 => float currentAmplitude => global float {1};
+            0 => float currentTimbre => global float {2};
+            0.1 => float slew;
+
+
+            fun void AssignParams()
+            {{
+                while( true )
+                {{
+                    // slew these params
+                    if( !Math.isnan({0}) ) {{ slew * ( {0} - currentFreq ) +=> currentFreq; }}
+                    if( !Math.isnan({1}) ) {{ slew * ( {1} - currentAmplitude ) +=> currentAmplitude; }}
+                    if( !Math.isnan({2}) ) {{ slew * ( {2} - currentTimbre ) +=> currentTimbre; }}
+                    currentFreq => t.freq;
+                    currentAmplitude => t.gain;
+                    Math.min( 50 + currentTimbre*10000, 18000 ) => l.freq;
+                    10::ms => now;
+                }}
+            }}
+
+            spork ~ AssignParams();
+            while( true ) {{ 1::second => now; }}
+        ", myPitch, myAmplitude, myTimbre ) );
+
+        currentAmplitude = 0;
+        currentPitch = 100;
+        currentTimbre = 0;
+        amOn = false;
+    }
+
+    public void TurnOn()
+    {
+        amOn = true;
+        SetAmplitude( currentAmplitude );
+    }
+    
+    public void TurnOff()
+    {
+        amOn = false;
+        SetAmplitude( currentAmplitude );
+    }
+
+    public void SetAmplitude( float a )
+    {
+        currentAmplitude = a;
+        if( amOn )
+        {
+            myChuck.SetFloat( myAmplitude, currentAmplitude );
+        }
+        else
+        {
+            myChuck.SetFloat( myAmplitude, 0 );
+        }
+    }
+
+    public void SetPitch( float p )
+    {
+        currentPitch = p;
+        myChuck.SetFloat( myPitch, currentPitch );
+    }
+
+    public void SetTimbre( float t )
+    {
+        currentTimbre = t;
+        myChuck.SetFloat( myTimbre, currentTimbre );
+    }
+}
---
+++ Assets/Scripts/Sound/CommunicateSynthMapping.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class CommunicateSynthMapping : MonoBehaviour
+{
+    // TODO: make photon-ready
+    //public CommunicateSynth synthPrefab;
+    //private static CommunicateSynth myCommunicator;
+    public CommunicateSynth myCommunicator;
+
+    public SteamVR_Action_Boolean turnOnCommunicator;
+    private SteamVR_Input_Sources handType;
+    private SteamVR_Behaviour_Pose pose;
+
+    public enum Mode { RecordExample, PlaybackExamples, ManualMapping1, ManualMapping2 };
+    public Mode myMode = Mode.ManualMapping1;
+    private bool amOn = false;
+    
+    void Awake()
+    {
+        pose = GetComponent<SteamVR_Behaviour_Pose>();
+        handType = pose.inputSource;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( myMode != Mode.RecordExample )
+        {
+            if( turnOnCommunicator.GetStateDown( handType ) ) 
+            {
+                myCommunicator.TurnOn();
+                amOn = true;
+            }
+            if( turnOnCommunicator.GetStateUp( handType ) )
+            {
+                myCommunicator.TurnOff();
+                amOn = false;
+            }
+        }
+
+        if( amOn )
+        {
+            switch( myMode )
+            {
+                case Mode.ManualMapping1:
+                    // speed = volume
+                    float volume = pose.GetVelocity().magnitude.PowMapClamp( 0, 4, 0, 1, 0.5f );
+                    
+                    // euler x = pitch
+                    Vector3 euler = GetModifiedEulerAngles();
+                    float pitch = euler.x.PowMapClamp( -80, 80, 1200, 100, 0.5f );
+
+                    // euler z = timbre
+                    float timbre = euler.z.PowMapClamp( -80, 80, 0, 1, 1.5f );
+
+                    myCommunicator.SetPitch( pitch );
+                    myCommunicator.SetTimbre( timbre );
+                    myCommunicator.SetAmplitude( volume );
+                    break;
+                case Mode.ManualMapping2:
+                    // speed = timbre
+                    // constant volume
+                    // ?? = pitch
+                    break;
+                case Mode.PlaybackExamples:
+                default:
+                    break;
+            }
+        }
+    }
+
+    private Vector3 GetModifiedEulerAngles()
+    {
+        Vector3 euler = transform.localEulerAngles;
+        if( euler.x >= 180 ) { euler.x -= 360; }
+        if( euler.y >= 180 ) { euler.y -= 360; }
+        if( euler.z >= 180 ) { euler.z -= 360; }
+        return euler;
+    }
+}

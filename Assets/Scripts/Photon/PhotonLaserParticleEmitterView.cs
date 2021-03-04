d7c21b47de25b980d747676cb0d1794c04be9aa6
Jack Atherton <atherton.lawrence@gmail.com>
2021-03-03 16:24:53 -0800

    test laser networking

---
+++ Assets/Scripts/Photon/PhotonLaserParticleEmitterView.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonLaserParticleEmitterView : MonoBehaviour , IPunObservable
+{
+    private DrawInAirController myDrawer;
+
+    public void Init( DrawInAirController c )
+    {
+        myDrawer = c;
+    }
+
+    void IPunObservable.OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )
+    {
+        // Write to others
+        if( stream.IsWriting )
+        {
+            bool enabled = myDrawer.GetEnabled();
+            float rate = myDrawer.GetEmissionRate();
+            float size = myDrawer.GetSize();
+            Color color = myDrawer.GetColor();
+
+            stream.SendNext( enabled );
+            stream.SendNext( rate );
+            stream.SendNext( size );
+            stream.SendNext( color.r );
+            stream.SendNext( color.g );
+            stream.SendNext( color.b );
+            stream.SendNext( color.a );
+        }
+        // Read from others
+        else
+        {
+            bool enabled = (bool) stream.ReceiveNext();
+            float rate = (float) stream.ReceiveNext();
+            float size = (float) stream.ReceiveNext();
+            float r = (float) stream.ReceiveNext();
+            float g = (float) stream.ReceiveNext();
+            float b = (float) stream.ReceiveNext();
+            float a = (float) stream.ReceiveNext();
+
+            myDrawer.SetEmissionRate( rate );
+            myDrawer.SetSize( size );
+            myDrawer.SetColor( new Color( r, g, b, a ) );
+            myDrawer.SetEnabled( enabled );
+        }
+    }
+}

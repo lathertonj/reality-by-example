9067d0f779ac5cca9cc8665097802f7f561e00fe
Jack Atherton <atherton.lawrence@gmail.com>
2021-03-12 16:06:33 -0800

    try direct copy for transform of laser

---
+++ Assets/Scripts/Photon/PhotonCopyTransformView.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonCopyTransformView : MonoBehaviour , IPunObservable
+{
+    public bool copyPosition = true;
+    public bool copyRotation = true;
+    public bool copyScale = false;
+
+
+    void IPunObservable.OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )
+    {
+        // Write to others
+        if (stream.IsWriting)
+        {
+            // first base position and rotation
+            if( copyPosition )
+            {
+                stream.SendNext( transform.position );
+            }
+
+            if( copyRotation )
+            {
+                stream.SendNext( transform.rotation );
+            }
+
+            if( copyScale )
+            {
+                stream.SendNext( transform.localScale );
+            }
+        }
+        // Read from others
+        else
+        {
+            if( copyPosition )
+            {
+                transform.position = (Vector3) stream.ReceiveNext();
+            }
+
+            if( copyRotation )
+            {
+                transform.rotation = (Quaternion) stream.ReceiveNext();
+            }
+
+            if( copyScale )
+            {
+                transform.localScale = (Vector3) stream.ReceiveNext();
+            }
+        }
+    }
+}

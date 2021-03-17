7c8ebf110687af1f66b7f135a81a8cc4591c6af1
Jack Atherton <atherton.lawrence@gmail.com>
2021-03-17 14:35:42 -0700

    evaporate and persist modes

--- Assets/Scripts/Photon/PhotonLaserParticleEmitterView.cs
+++ Assets/Scripts/Photon/PhotonLaserParticleEmitterView.cs
@@ -7,6 +7,7 @@ public class PhotonLaserParticleEmitterView : MonoBehaviour , IPunObservable
 {
     private DrawInAirController myDrawer = null;
     private ParticleSystem myParticles;
+    private PhotonView myView;
 
     public void Init( DrawInAirController c )
     {
@@ -16,6 +17,35 @@ public class PhotonLaserParticleEmitterView : MonoBehaviour , IPunObservable
     void Awake()
     {
         myParticles = GetComponent<ParticleSystem>();
+        myView = GetComponent<PhotonView>();
+    }
+
+    public void SetTrailDrawMode( DrawInAirController.TrailMode mode )
+    {
+
+        switch( mode )
+        {
+            case DrawInAirController.TrailMode.Evaporate:
+                if( myView ) { myView.RPC( "DrawLaserEvaporateMode", RpcTarget.All ); } else { DrawLaserEvaporateMode(); }
+                break;
+            case DrawInAirController.TrailMode.Persist:
+                if( myView ) { myView.RPC( "DrawLaserPersistMode", RpcTarget.All ); } else { DrawLaserPersistMode(); }
+                break;
+        }
+    }
+
+    [PunRPC]
+    public void DrawLaserEvaporateMode()
+    {
+        var main = myParticles.main;
+        main.ringBufferMode = ParticleSystemRingBufferMode.Disabled;
+    }
+
+    [PunRPC]
+    public void DrawLaserPersistMode()
+    {
+        var main = myParticles.main;
+        main.ringBufferMode = ParticleSystemRingBufferMode.LoopUntilReplaced;
     }
 
     void IPunObservable.OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )

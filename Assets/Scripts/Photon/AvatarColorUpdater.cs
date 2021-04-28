2e31b155f8d247ec97c05e11fcfe0b055a825fa5
Jack Atherton <atherton.lawrence@gmail.com>
2021-04-28 10:29:20 -0700

    be able to change avatar color

---
+++ Assets/Scripts/Photon/AvatarColorUpdater.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class AvatarColorUpdater : MonoBehaviour
+{
+    private static List<AvatarColorUpdater> avatars;
+    private MeshRenderer me;
+    private PhotonView myView;
+    public float colorAlpha = 0.6f;
+    
+    void Awake()
+    {
+        if( avatars == null )
+        {
+            avatars = new List<AvatarColorUpdater>();
+        }    
+        avatars.Add( this );
+
+        me = GetComponentInChildren<MeshRenderer>();
+        myView = GetComponent<PhotonView>();
+    }
+
+    private void UpdateColor( Color newColor )
+    {
+        newColor.a = colorAlpha;
+        me.material.color = newColor;
+    }
+
+    private bool BelongsToThisClient()
+    {
+        return myView.IsMine;
+    }
+
+    public static void UpdateColors( Color newColor )
+    {
+        foreach( AvatarColorUpdater avatar in avatars )
+        {
+            // only update the ones belonging to this client
+            if( avatar.BelongsToThisClient() )
+            {
+                avatar.UpdateColor( newColor );
+            }
+        }
+    }
+}
